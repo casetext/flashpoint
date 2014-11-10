2f2172981d7555a6e38a3270ec66edf82f63a531
Harry Schmidt <goldibex@casetext.com>
2014-11-10 15:25:27 -0800

    Don't use commonjs in Karma config

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -5,18 +5,15 @@ module.exports = function(config) {
 
   config.set({
     basePath: '..',
-    frameworks: ['mocha', 'chai', 'commonjs'],
+    frameworks: ['mocha', 'chai'],
     files: [
       { pattern: 'node_modules/angular/lib/angular.js', watched: false },
       { pattern: 'node_modules/angular-mocks/angular-mocks.js', watched: false },
       { pattern: 'node_modules/firebase/lib/firebase-web.js', watched: false },
-      { pattern: 'node_modules/fireproof/index.js', watched: false },
+      { pattern: 'node_modules/fireproof/dist/fireproof.js', watched: false },
       'src/**/*.js',
       'test/spec/**/*.js'
-    ],
-    preprocessors: {
-      'node_modules/fireproof/index.js': 'commonjs'
-    }
+    ]
   });
 
 };
