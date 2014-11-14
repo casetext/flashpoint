5fcd137db99910a745d26dfa34c8b6d46bcf7821
Harry Schmidt <goldibex@casetext.com>
2014-11-13 17:38:39 -0800

    Expose ServerValue

--- src/services/Fireproof.js
+++ src/services/Fireproof.js
@@ -3,6 +3,9 @@ angular.module('angular-fireproof.services.Fireproof', [])
 .factory('Firebase', function() {
   return Firebase;
 })
+.factory('ServerValue', function(Firebase) {
+  return Firebase.ServerValue;
+})
 .factory('Fireproof', function($timeout, $q) {
 
   Fireproof.setNextTick($timeout);
