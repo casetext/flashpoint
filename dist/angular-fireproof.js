6f10916704aa4084c8886f5e91b6c0275a30c75c
Harry Schmidt <goldibex@casetext.com>
2014-11-13 17:39:03 -0800

    build

--- dist/angular-fireproof.js
+++ dist/angular-fireproof.js
@@ -584,6 +584,9 @@
   .factory('Firebase', function() {
     return Firebase;
   })
+  .factory('ServerValue', function(Firebase) {
+    return Firebase.ServerValue;
+  })
   .factory('Fireproof', function($timeout, $q) {
   
     Fireproof.setNextTick($timeout);
