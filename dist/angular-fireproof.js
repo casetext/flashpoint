97d0630405e4d88527d71b837bd52d47b4b431f6
Harry Schmidt <goldibex@casetext.com>
2014-11-17 12:18:28 -0800

    Export the damn directives

--- dist/angular-fireproof.js
+++ dist/angular-fireproof.js
@@ -189,85 +189,6 @@
   });
   
   
-  angular.module('angular-fireproof.services.Fireproof', [])
-  .factory('Firebase', function() {
-    return Firebase;
-  })
-  .factory('ServerValue', function(Firebase) {
-    return Firebase.ServerValue;
-  })
-  .factory('Fireproof', function($timeout, $q) {
-  
-    Fireproof.setNextTick($timeout);
-    Fireproof.bless($q);
-  
-    return Fireproof;
-  
-  });
-  
-  
-  angular.module('angular-fireproof.services.status', [])
-  .service('_fireproofStatus', function($timeout, $rootScope) {
-  
-    var service = this;
-  
-    function reset() {
-  
-      service.loaded = false;
-      service.running = {};
-      service.finished = {};
-  
-    }
-  
-    reset();
-  
-    service.start = function(path) {
-  
-      if (service.running[path]) {
-        service.running[path]++;
-      } else {
-        service.running[path] = 1;
-      }
-  
-    };
-  
-    service.finish = function(path) {
-  
-      if (!angular.isDefined(service.running[path])) {
-        throw new Error('Path ' + path + ' is not actually running right now -- race condition?');
-      }
-  
-      service.running[path]--;
-      if (service.running[path] === 0) {
-  
-        delete service.running[path];
-        service.finished[path] = true;
-  
-        $timeout(function() {
-  
-          // how many operations are left?
-          var done = true;
-          angular.forEach(service.running, function() {
-            done = false;
-          });
-  
-          if (done && !service.loaded) {
-  
-            service.loaded = true;
-            // signal loaded
-            $rootScope.$broadcast('angular-fireproof:loaded', service);
-  
-          }
-  
-        }, 10);
-  
-      }
-  
-    };
-  
-  });
-  
-  
   angular.module('angular-fireproof.directives.authClick', [
     'angular-fireproof.directives.firebase'
   ])
@@ -1034,6 +955,85 @@
   
   });
   
+  
+  
+  angular.module('angular-fireproof.services.Fireproof', [])
+  .factory('Firebase', function() {
+    return Firebase;
+  })
+  .factory('ServerValue', function(Firebase) {
+    return Firebase.ServerValue;
+  })
+  .factory('Fireproof', function($timeout, $q) {
+  
+    Fireproof.setNextTick($timeout);
+    Fireproof.bless($q);
+  
+    return Fireproof;
+  
+  });
+  
+  
+  angular.module('angular-fireproof.services.status', [])
+  .service('_fireproofStatus', function($timeout, $rootScope) {
+  
+    var service = this;
+  
+    function reset() {
+  
+      service.loaded = false;
+      service.running = {};
+      service.finished = {};
+  
+    }
+  
+    reset();
+  
+    service.start = function(path) {
+  
+      if (service.running[path]) {
+        service.running[path]++;
+      } else {
+        service.running[path] = 1;
+      }
+  
+    };
+  
+    service.finish = function(path) {
+  
+      if (!angular.isDefined(service.running[path])) {
+        throw new Error('Path ' + path + ' is not actually running right now -- race condition?');
+      }
+  
+      service.running[path]--;
+      if (service.running[path] === 0) {
+  
+        delete service.running[path];
+        service.finished[path] = true;
+  
+        $timeout(function() {
+  
+          // how many operations are left?
+          var done = true;
+          angular.forEach(service.running, function() {
+            done = false;
+          });
+  
+          if (done && !service.loaded) {
+  
+            service.loaded = true;
+            // signal loaded
+            $rootScope.$broadcast('angular-fireproof:loaded', service);
+  
+          }
+  
+        }, 10);
+  
+      }
+  
+    };
+  
+  });
   
 
 }));
--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ angular.module('angular-fireproof', [
   'angular-fireproof.directives.firebase',
   'angular-fireproof.directives.fpBind',
   'angular-fireproof.directives.fpPage',
+  'angular-fireproof.directives.authIf',
+  'angular-fireproof.directives.authShow',
+  'angular-fireproof.directives.authClick',
   'angular-fireproof.services.Fireproof'
 ]);
