90bdaf06f8bccb893c8849a410c484e1698ce71a
Harry Schmidt <goldibex@casetext.com>
2014-11-17 12:19:19 -0800

    build

--- dist/angular-fireproof.js
+++ dist/angular-fireproof.js
@@ -23,6 +23,9 @@
     'angular-fireproof.directives.firebase',
     'angular-fireproof.directives.fpBind',
     'angular-fireproof.directives.fpPage',
+    'angular-fireproof.directives.authIf',
+    'angular-fireproof.directives.authShow',
+    'angular-fireproof.directives.authClick',
     'angular-fireproof.services.Fireproof'
   ]);
   
